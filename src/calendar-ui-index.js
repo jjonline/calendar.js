408a68e0078c37e3e14a9c8644c7880134c3399d
ben <ben@opensource.ext>
2023-05-20 17:50:27 +0800

    *优化代码!
    Signed-off-by: ben <ben@opensource.ext>

--- src/calendar-ui-index.js
+++ src/calendar-ui-index.js
@@ -1,7 +1,10 @@
 import {TCalendarUI} from "./calendar-ui";
 import calendar from "./index";
 
-const calendarUI = TCalendarUI.createCalendarUI();
-calendar.UI = calendarUI;
+
+calendar.createUI = function(dest, options){
+    return TCalendarUI.createCalendarUI(dest, options);
+}
+
 
 export default calendar;
